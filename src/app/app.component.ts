c9d6469fb3a690fc862335d6d9c945cebf68fe6d
ndongba <ndongba01@gmail.com>
2024-07-27 15:52:57 +0000

    Fonctionnalité ajout contact terminée

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,35 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators, ReactiveFormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ReactiveFormsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'ContactsAngular';
+  form: FormGroup;
+
+  constructor() {
+    this.form = new FormGroup({
+      nom: new FormControl('', Validators.required),
+      prenom: new FormControl('', Validators.required),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      telephone: new FormControl('', Validators.required),
+      etat: new FormControl('', Validators.required),
+      message: new FormControl('', Validators.required),
+    });
+  }
+
+  onSubmit() {
+    if (this.form.valid) {
+      // Enregistrement des données dans le Local Storage
+      localStorage.setItem('formData', JSON.stringify(this.form.value));
+      
+      // Affichage des données dans la console
+     // console.log(this.form.value);
+    }
+  }
 }
